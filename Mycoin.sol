62e36efbc8f835a4bfafc08025146b7b9e288ea6
mjlover05 <93177731+mjlover05@users.noreply.github.com>
2023-07-20 00:43:20 +0530

    Update Mycoin.sol

--- Mycoin.sol
+++ Mycoin.sol
@@ -1,1 +1,30 @@
-k
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+//This line imports the ERC20 contract from the OpenZeppelin library. 
+//The OpenZeppelin library provides reusable and secure smart contract components for building decentralized applications (dApps) on the Ethereum blockchain.
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyCoin is ERC20 {
+    address private owner;
+
+    constructor() ERC20("Bitcoin", "BC") {
+        owner = msg.sender;
+        _mint(msg.sender, 10**decimals());
+    }
+
+    modifier Owner() {
+        require(msg.sender == owner, "Only Owner can access this.");
+        _;
+    }
+
+    function mint(address account, uint256 value) external Owner {
+        _mint(account, value);
+    }
+
+    function burn(uint256 value) external {
+        _burn(msg.sender, value);
+    }
+
+}
